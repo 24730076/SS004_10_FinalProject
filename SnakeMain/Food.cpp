178f1cfc2837ef0c9a27fe64235b252f42899877
24730037 <24730037@ms.uit.edu.vn>
2024-11-20 20:31:05 +0700

    develop method viewscore init

--- SnakeMain/Food.cpp
+++ SnakeMain/Food.cpp
@@ -1,7 +1,9 @@
 ﻿﻿#include "Food.h"
 
 Food::Food(int gameWidth, int gameHeight) : width(gameWidth), height(gameHeight) {
-    
+    srand(static_cast<unsigned>(time(0)));
+    x = rand() % width;
+    y = rand() % height;
 }
 
 void Food::GenerateNewPosition(const std::vector<std::pair<int, int>>& snakeBody) {
