0165a624f5528200ae3918f494491ef02ca000d0
24730014-TranNgocThaiBinh <24730014@ms.uit.edu.vn>
2024-11-20 19:51:48 +0700

    develop function viewscore init

--- SnakeMain/Game.cpp
+++ SnakeMain/Game.cpp
@@ -51,11 +51,41 @@ void Game::Init() {
 }
 
 void Game::Update() {
-    
+    lastTail = snake.GetBody().back();
+
+    snake.Move();
+
+    if (snake.GetHeadPosition() == food.GetPosition()) {
+        snake.Grow();
+        food.GenerateNewPosition(snake.GetBody());
+        score += 10;
+    }
+
+    if (snake.CheckCollision(width, height)) {
+        isRunning = false;
+    }
 }
 
 void Game::Render() {
-    
+    GotoXY(lastTail.first + 1, lastTail.second + 1); // +1 để bù viền
+    std::cout << " ";
+
+    auto head = snake.GetHeadPosition();
+    GotoXY(head.first + 1, head.second + 1);
+    std::cout << "O";
+
+    const auto& body = snake.GetBody();
+    for (size_t i = 1; i < body.size(); ++i) {
+        auto segment = body[i];
+        GotoXY(segment.first + 1, segment.second + 1);
+        std::cout << "o";
+    }
+    auto foodPos = food.GetPosition();
+    GotoXY(foodPos.first + 1, foodPos.second + 1);
+    std::cout << "F";
+
+    GotoXY(0, height + 3);
+    std::cout << "Score: " << score << "\n";
 }
 
 void Game::Run() {
